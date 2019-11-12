d8a4b0412a0a452b43564a3c46966fc440af7c13
Casneil Simpson <casneil.simpson@gmail.com>
2019-11-12 22:01:01 +0100

    added functionality to Votes Component

--- src/components/Votes.js
+++ src/components/Votes.js
@@ -1,10 +1,14 @@
 import React from "react";
+import { connect } from "react-redux";
+
+// MUI Imports
 import PropTypes from "prop-types";
 import { makeStyles } from "@material-ui/core/styles";
 import Rating from "@material-ui/lab/Rating";
 import Tooltip from "@material-ui/core/Tooltip";
 import Typography from "@material-ui/core/Typography";
 import Box from "@material-ui/core/Box";
+import Avatar from "@material-ui/core/Avatar";
 
 const labels = {
   0.5: "Useless",
@@ -17,6 +21,48 @@ const labels = {
   4: "Good+",
   4.5: "Excellent",
   5: "Excellent+"
+};
+
+const useStyles = makeStyles({
+  rating1: {
+    width: 200,
+    display: "flex",
+    alignItems: "center"
+  },
+  bigAvatar: {
+    margin: 10,
+    width: 60,
+    height: 60
+  }
+});
+
+// image={users.find(user => user.id === qs.author).avatarURL}
+
+const Votes = ({ qs, users, authedUser }) => {
+  console.log("UUUUUSEEEERs", users);
+  const value = 2;
+
+  const classes = useStyles();
+
+  return (
+    <div className={classes.rating}>
+      <Box component="fieldset" mb={3} borderColor="transparent">
+        <Typography component="legend">Ratings</Typography>
+
+        {users.map(user => (
+          <div>
+            <Avatar
+              alt="Remy Sharp"
+              src={user.avatarURL}
+              className={classes.bigAvatar}
+            />
+            {user.name}
+            <Rating name="read-only" value={value} readOnly />
+          </div>
+        ))}
+      </Box>
+    </div>
+  );
 };
 
 function IconContainer(props) {
@@ -32,46 +78,14 @@ IconContainer.propTypes = {
   value: PropTypes.number.isRequired
 };
 
-const useStyles = makeStyles({
-  rating1: {
-    width: 200,
-    display: "flex",
-    alignItems: "center"
-  }
-});
+function mapStateToProps({ authedUser, users, questions }, { id }) {
+  const qs = questions[id];
 
-const Votes = () => {
-  const value = 2;
-  const [hover, setHover] = React.useState(-1);
-  const classes = useStyles();
+  return {
+    qs,
+    users: Object.values(users),
+    authedUser
+  };
+}
 
-  return (
-    <div>
-      <Box component="fieldset" mb={3} borderColor="transparent">
-        <Typography component="legend">Side</Typography>
-        <div className={classes.rating1}>
-          <Rating
-            name="hover-side"
-            value={value}
-            precision={0.5}
-            onChangeActive={(event, newHover) => {
-              setHover(newHover);
-            }}
-          />
-          <Box ml={2}>{labels[hover !== -1 ? hover : value]}</Box>
-        </div>
-      </Box>
-      <Box component="fieldset" mb={3} borderColor="transparent">
-        <Typography component="legend">Tooltip</Typography>
-        <Rating
-          name="hover-tooltip"
-          value={value}
-          precision={0.5}
-          IconContainerComponent={IconContainer}
-        />
-      </Box>
-    </div>
-  );
-};
-
-export default Votes;
+export default connect(mapStateToProps)(Votes);
