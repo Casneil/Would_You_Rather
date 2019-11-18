e9323721c0dd6dc1bae0c37689f583974a4fae81
Casneil Simpson <casneil.simpson@gmail.com>
2019-11-18 11:39:37 +0100

    Updates

---
+++ precache-manifest.b946a0004a2226e080af0ec9cb7eec97.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "610c8a5ffcba95d6127fe0941a726a37",
+    "url": "/Would_You_Rather/index.html"
+  },
+  {
+    "revision": "e7b58e5eda1bd641f60d",
+    "url": "/Would_You_Rather/static/css/main.7f0e9e84.chunk.css"
+  },
+  {
+    "revision": "a05e73d2eae65b325c81",
+    "url": "/Would_You_Rather/static/js/2.d52cf437.chunk.js"
+  },
+  {
+    "revision": "e7b58e5eda1bd641f60d",
+    "url": "/Would_You_Rather/static/js/main.dee30fd2.chunk.js"
+  },
+  {
+    "revision": "8d5f04fcbe053c0ce5a7",
+    "url": "/Would_You_Rather/static/js/runtime-main.ed79ba51.js"
+  },
+  {
+    "revision": "bb259ac178d725b37b2cfade21a1315c",
+    "url": "/Would_You_Rather/static/media/ben.bb259ac1.svg"
+  },
+  {
+    "revision": "cdc2ab069f513e85de429b9342b51c7a",
+    "url": "/Would_You_Rather/static/media/casneil.cdc2ab06.svg"
+  },
+  {
+    "revision": "7963ff18f5c2dd79178dbac27f849df6",
+    "url": "/Would_You_Rather/static/media/cover.7963ff18.jpg"
+  },
+  {
+    "revision": "bea7f618eb07dd4863343fba2e60eb2b",
+    "url": "/Would_You_Rather/static/media/linda.bea7f618.svg"
+  },
+  {
+    "revision": "8d2dc4526259694e55186d30282748bb",
+    "url": "/Would_You_Rather/static/media/quesPic.8d2dc452.jpg"
+  },
+  {
+    "revision": "614cf05474ced22f06269d8a73fce797",
+    "url": "/Would_You_Rather/static/media/tom.614cf054.svg"
+  }
+]);
